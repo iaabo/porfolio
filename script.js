b812638dacf4e94840a116c9b07166f136bb718a
iaabo <insbarroso@gmail.com>
2021-01-03 21:03:19 +0000

    done

--- script.js
+++ script.js
@@ -23,11 +23,9 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-
-
 /* Scroll Sections*/
 
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
 window.addEventListener("scroll", scrollActive);
 
@@ -51,3 +49,27 @@ function scrollActive() {
   });
 }
 
+/* Scroll Animation */
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".home__title", {});
+sr.reveal(".home__scroll", { delay: 200 });
+sr.reveal(".home__img", { origin: "right", delay: 400 });
+
+sr.reveal(".about__img", { delay: 500 });
+sr.reveal(".about__subtitle", { delay: 300 });
+sr.reveal(".about__profession", { delay: 400 });
+sr.reveal(".about__text", { delay: 500 });
+sr.reveal(".about__social-icon", { delay: 600, interval: 200 });
+
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills__img", { delay: 400 });
+
+sr.reveal(".portfolio__img", { interval: 200 });
